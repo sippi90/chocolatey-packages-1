7abb95f0e008047b59186928d9e645adb4eceac9
Jean-Philippe Steinmetz <caskater47@gmail.com>
2017-06-16 23:23:03 -0700

    Fixing more issues with p4 package installer

--- automatic/p4/tools/chocolateyInstall.ps1
+++ automatic/p4/tools/chocolateyInstall.ps1
@@ -1,11 +1,4 @@
-﻿﻿$ErrorActionPreference = 'Stop';
-$packageName = 'p4'
-$url32 = 'http://filehost.perforce.com/perforce/r17.1/bin.ntx86/helix-p4-x86.exe'
-$url64 = 'http://filehost.perforce.com/perforce/r17.1/bin.ntx64/helix-p4-x64.exe'
-$checksum32  = 'a5466b6644773dab94f6fdee01b977e8473421f498acdb4947fca109a176b60a'
-$checksum64  = '0a65b8b0807b263765eee73fcb7d9b4d7cedf557f4dcc8a40fae7fdc9068b132'
-
-$packageName = 'p4'
+﻿$packageName = 'p4'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32       = 'http://filehost.perforce.com/perforce/r17.1/bin.ntx86/helix-p4-x86.exe'
 $url64       = 'http://filehost.perforce.com/perforce/r17.1/bin.ntx64/helix-p4-x64.exe'
