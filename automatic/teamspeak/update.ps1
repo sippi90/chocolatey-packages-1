b93dfa34115ccf2902f7ee7583355e10b6603a00
sippi90 <sippi90@users.noreply.github.com>
2018-12-26 21:30:48 +0100

    maybe a bit more stable

--- automatic/teamspeak/update.ps1
+++ automatic/teamspeak/update.ps1
@@ -1,8 +1,6 @@
 import-module au
 
-$baseName = 'TeamSpeak3-Client'
-$downloadsUrl = 'https://www.teamspeak.com/en/downloads'
-$mirror = 'http://dl.4players.de/ts/releases/'
+$releases = 'https://www.teamspeak.com/en/downloads/'
 
 function global:au_SearchReplace {
     @{
@@ -16,30 +14,13 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$downloads = Invoke-WebRequest $downloadsUrl
-	
-	# All of the interesting information is contained in a div of class "platform windows"
-	$windowsElement = $downloads.ParsedHtml.getElementsByTagName("div") | Where{ $_.className -eq 'platform mb-5 windows' }
-	# The windows div contains two more elements of class type 'row'. These are 32-bit and 64-bit respectively.
-	$version = ($windowsElement.getElementsByTagName("span") | Where{ $_.className -eq 'version' } | Select -First 1).innerText.Trim()
-	
-	# Get the 32-bit installer url and checksum
-	$checksum32 = ($windowsElement.getElementsByTagName("div") | Where{ $_.className -eq 'checksum' } | Select -First 1).innerText
-	$url32 = $mirror + '/' + $version + '/' + $baseName + '-win32-' + $version + '.exe'
-	
-	# Get the 64-bit installer url and checksum
-	$checksum64 = ($windowsElement.getElementsByTagName("div") | Where{ $_.className -eq 'checksum' } | Select -Skip 1 | Select -First 1).innerText
-	$url64 = $mirror + '/' + $version + '/' + $baseName + '-win64-' + $version + '.exe'
-	
-	# The sha256 checksums need to be parsed a bit
-	$regex = [regex] 'SHA256: [a-zA-Z0-9]+'
-	$matches = $regex.Matches($checksum32)
-	$sum32 = $matches[0] -split ' ' | Select -Last 1
-	$matches = $regex.Matches($checksum64)
-	$sum64 = $matches[0] -split ' ' | Select -Last 1
-
-	$Latest = @{ URL32 = $url32; Checksum32 = $sum32; URL64 = $url64; Checksum64 = $sum64; Version = $version }
-    return $Latest
+  $downloads = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $url32 =  ($downloads.links | Where-Object {$_.outerHTML -like '*client-win32*'}).href
+  $url64 =  ($downloads.links | Where-Object {$_.outerHTML -like '*client-win64*'}).href
+  $version = [regex]::match($url64,'[0-9]+(\.[0-9]+)*').value
+  
+  return @{ URL64 = $url64; URL32 = $url32; Version = $version }
+ 
 }
 
 update
